8de933a39102d75808b510dfb29092559a74e657
Babak Daliran <babak.daliran13@gmail.com>
2026-02-16 10:59:47 +0330

    remove migrations part-2 commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,14 +24,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $categories = Category::query()->with('childCategory.childCategory')->where('parent_id', 0)->get();
-        $banners = Cache::remember('banners',60*60*24*10,function (){
-            return Banner::query()->get();
-        });
-        View::share([
-            'categories'=>$categories,
-            'banners'=>$banners
-        ]);
-        Paginator::useBootstrap();
+//        $categories = Category::query()->with('childCategory.childCategory')->where('parent_id', 0)->get();
+//        $banners = Cache::remember('banners',60*60*24*10,function (){
+//            return Banner::query()->get();
+//        });
+//        View::share([
+//            'categories'=>$categories,
+//            'banners'=>$banners
+//        ]);
+//        Paginator::useBootstrap();
     }
 }
