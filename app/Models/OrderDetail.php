f17f072360fe00d9b6ff8094b5aa56b0ceab372e
Babak Daliran <babak.daliran13@gmail.com>
2026-02-17 15:19:14 +0330

    edit orderDetail part-1 commit

--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -16,35 +16,44 @@ class OrderDetail extends Model
         'discount',
         'description',
         'status',
+        'seller_share',
+        'site_share'
     ];
 
     public function order()
     {
         return $this->belongsTo(Order::class);
     }
+
     public function product()
     {
         return $this->belongsTo(Product::class);
     }
 
-    public static function createOrderDetail($order,$cart,$product_price)
+    public static function createOrderDetail($order, $cart, $product)
     {
+        $commission_percent = 20;
+        $siteShare = ($product->price * $commission_percent) / 100;
+        $sellerShare = $product->price - $siteShare;
+
         return OrderDetail::query()->create([
-            'seller_id'=>$product_price->user_id,
-            'order_id'=>$order->id,
-            'product_id'=>$cart->product_id,
-            'main_price'=>$product_price->main_price,
-            'price'=>$product_price->price,
-            'discount'=>$product_price->discount,
-            'status'=>OrderDetailStatus::Waiting->value,
+            'seller_id' => $product->user_id,
+            'order_id' => $order->id,
+            'product_id' => $cart->product_id,
+            'main_price' => $product->main_price,
+            'price' => $product->price,
+            'discount' => $product->discount,
+            'status' => OrderDetailStatus::Waiting->value,
+            'seller_share' => $sellerShare,         // سهم خالص فروشنده
+            'site_share' => $siteShare,           // سهم پلتفرم (پورسانت)
         ]);
     }
 
     public static function calculateMoneyForCommission($order_detail)
     {
-        if ($order_detail->product->category->commission){
+        if ($order_detail->product->category->commission) {
             return $order_detail->price - ((($order_detail->product->category->commission->commission_percent) * $order_detail->price) / 100);
-        }else{
+        } else {
             return $order_detail->price;
         }
     }
