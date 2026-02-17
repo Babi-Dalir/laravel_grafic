7e0afd2ce6045c21bf7df166cd90b70acdd247aa
Babak Daliran <babak.daliran13@gmail.com>
2026-02-17 15:01:07 +0330

    create migration and add recorde commit

--- app/Models/Downloads.php
+++ app/Models/Downloads.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Downloads extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'order_detail_id',
+        'token',
+        'download_count',
+        'max_download',
+        'status',
+        'expire_at',
+        'ip_address',
+        'user_agent',
+    ];
 }
