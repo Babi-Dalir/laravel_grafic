648d9c98d628e688d52c433bcd2cb8e5ec68c159
Babak Daliran <babak.daliran13@gmail.com>
2026-05-30 12:19:33 +0330

    change home controller

--- app/Http/Controllers/FrontEnd/HomeController.php
+++ app/Http/Controllers/FrontEnd/HomeController.php
@@ -22,44 +22,54 @@ class HomeController extends Controller
     {
         $now = now();
 
-        $sliders = Cache::remember('sliders',60*60*24*10,function (){
+        $sliders = Cache::remember('sliders', 60 * 60 * 24 * 10, function () {
             return Slider::query()
-                ->where('status',SliderStatus::Active->value)->get();
+                ->where('status', SliderStatus::Active->value)->get();
         });
         $most_sold = Product::query()
-            ->with(['category','campaignTargets.campaign'])
-            ->orderBy('sold','DESC')
+            ->with(['category', 'campaignTargets.campaign'])
+            ->orderBy('sold', 'DESC')
             ->get();
         $newest_products = Product::query()
             ->with(['category', 'campaignTargets.campaign'])
-            ->orderBy('created_at','DESC')
+            ->orderBy('created_at', 'DESC')
             ->get();
         $spacial_products = Product::query()
             ->with(['category', 'campaignTargets.campaign'])
-            ->whereHas('campaignTargets.campaign', function ($query) use ($now) {
-                $query->where('status',DiscountCampaignStatus::Active->value)
-                    ->where('starts_at', '<=', $now)
-                    ->where(function ($q) use ($now) {
-                        $q->whereNull('expires_at')->orWhere('expires_at', '>=', $now);
+            ->where(function ($q) use ($now) {
+                $q->whereHas('campaignTargets.campaign', function ($query) use ($now) {
+                    $query->where('status', DiscountCampaignStatus::Active->value)
+                        ->where('starts_at', '<=', $now)
+                        ->where(function ($e) use ($now) {
+                            $e->whereNull('expires_at')->orWhere('expires_at', '>=', $now);
+                        });
+                })
+                    ->orWhere(function ($query) {
+                        // محصولات با تخفیف مستقیم
+                        $query->whereRaw('main_price > 0');
                     });
             })
-            ->take(10)
-            ->get();
+            ->get()
+            ->filter(function ($product) {
+                return $product->hasDiscount();
+            })
+            ->take(10);
+
         // پیشنهاد لحظه ای
 
-         $instant_offers = Product::smartOffer()
-        ->limit(9)
-        ->get();
+        $instant_offers = Product::smartOffer()
+            ->limit(9)
+            ->get();
 
-        return view('frontend.index',compact('sliders'
-            ,'most_sold','spacial_products','newest_products','instant_offers'));
+        return view('frontend.index', compact('sliders'
+            , 'most_sold', 'spacial_products', 'newest_products', 'instant_offers'));
     }
 
     public function userCart()
     {
         $carts_count = UserCart::query()
-            ->where('user_id',auth()->id())
-            ->where('type',CartType::Main->value)->count();
-        return view('frontend.user_cart',compact('carts_count'));
+            ->where('user_id', auth()->id())
+            ->where('type', CartType::Main->value)->count();
+        return view('frontend.user_cart', compact('carts_count'));
     }
 }
